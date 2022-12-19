91860bf1d36adcaaa35743821dcf7d10d59d27ec
AdamFull <logotipick@gmail.com>
2022-12-19 22:49:10 +0400

    interface update

--- source/constants.h
+++ source/constants.h
@@ -3,7 +3,7 @@
 namespace testing
 {
 	// Constants
-	constexpr const size_t test_times = 100; // Number of algorithm execution
+	constexpr const size_t test_times = 1000; // Number of algorithm execution
 	constexpr const char* tests_output_folder = "tests_output"; // Output images folder
 	constexpr const char* output_image_format = "png"; // Output image format
 	constexpr const bool save_output_results = true; // Enable saving output images
